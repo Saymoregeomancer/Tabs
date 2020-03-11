ebdb29ce189529f7ead43e91e1a8673ae5286e01
vadim/ <saymoregeomancer@gmail.com>
2020-03-11 13:16:05 +0200

    timer works

--- js/script.js
+++ js/script.js
@@ -34,4 +34,55 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    // Timer
+
+    let deadline = '2020-03-12';
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes': minutes,
+            'seconds' : seconds,
+        };
+    }
+
+    function setClock (id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t.seconds < 10 && t.seconds > 0) {
+                seconds.textContent = '0' + t.seconds;
+            }
+            if (t.minutes < 10 && t.minutes > 0) {
+                minutes.textContent = '0'+t.minutes;
+            }
+            if (t.hours < 10 && t.hours > 0) {
+                hours.textContent = '0' + t.hours;
+            }
+
+            if (t.total <= 0) {
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 });
